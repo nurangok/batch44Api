64322ae4c11875d826571fc921421f58b7933462
nurangok <nurlibreville@gmail.com>
2022-03-06 22:24:49 +0100

    Get01

--- src/test/java/get_http_request/GetRequest01.java
+++ src/test/java/get_http_request/GetRequest01.java
@@ -8,29 +8,26 @@ import static io.restassured.RestAssured.given;
 
 public class GetRequest01 {
 
+
     @Test
     public void test01(){
 
-        String url = "https://restful-booker.herokuapp.com/booking";
-
-        Response response = given().when().get(url);
-
+        String url="https://restful-booker.herokuapp.com/booking";
+        Response response= given().when().get(url);
         //response.prettyPrint();
 
-        System.out.println("Status Code: " + response.statusCode());
-        System.out.println("Content Type: " + response.contentType());
-        System.out.println("Status Line: " + response.statusLine());
-        System.out.println("Test Zamani: " + response.time());
+        System.out.println("status code =" + response.statusCode());
+        System.out.println("status line= " + response.statusLine());
+        System.out.println("content type= " + response.contentType());
+        System.out.println("test suresi= " + response.time());
 
         Assert.assertEquals(200, response.statusCode());
         Assert.assertEquals("application/json; charset=utf-8", response.contentType());
-        Assert.assertEquals("HTTP/1.1 200 OK", response.statusLine());
 
-        response
-                .then()
-                .assertThat()
+        response.
+                then().assertThat()
                 .statusCode(200)
-                .contentType("application/json; charset=utf-8")
-                .statusLine("HTTP/1.1 200 OK");
+                .contentType("application/json; charset=utf-8");
+
     }
 }
