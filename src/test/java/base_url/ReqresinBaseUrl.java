8b9b7dd8292b7859eac93b40f2479788faa5ce96
nurangok <nurlibreville@gmail.com>
2022-03-10 19:46:44 +0100

    reqresinbase

--- src/test/java/base_url/ReqresinBaseUrl.java
+++ src/test/java/base_url/ReqresinBaseUrl.java
@@ -5,10 +5,12 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class ReqresinBaseUrl {
+
     protected RequestSpecification spec01;
 
     @Before
     public void setUp(){
+
         spec01 = new RequestSpecBuilder().setBaseUri("https://reqres.in").build();
     }
 }
