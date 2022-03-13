1563a3fa5719c75d46bf13b7d642a137d272b709
nurangok <nurlibreville@gmail.com>
2022-03-13 22:32:17 +0100

    getrequest11

--- src/test/java/get_http_request/day04/GetRequest11.java
+++ src/test/java/get_http_request/day04/GetRequest11.java
@@ -1,5 +1,7 @@
 package get_http_request.day04;
 
+
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 
@@ -8,12 +10,12 @@ import static io.restassured.RestAssured.given;
 public class GetRequest11 {
 
     String endPoint = "http://www.gmibank.com/api/tp-customers";
-    String bearerToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtdXJhdHRhbmMiLCJhdXRoIjoiUk9MRV9BRE1JTiIsImV4cCI6MTY0NjUwNTU1NH0.ySbwAfpdtEWsWMucCoyBB5ND9Cu1jyD5rwNLSqF6GQu0XfM0LwCg6PerxCkbJCVFJ7CslucH5VOWEenRK2HQ7w";
+    String bearerToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRjaDQ0YXBpIiwiYXV0aCI6IlJPTEVfQ1VTVE9NRVIiLCJleHAiOjE2NDcwOTgyMDR9.MQksKVvs4cqHbVngthUJSwGVf04pg2Iw4zcFhDXKy5whW4fjBaYa5Ujz2qcx9x__K-BOxvff58HI6q9oIzHPOw";
 
     @Test
     public void test(){
 
-        Response response = given()
+        Response response = given().accept(ContentType.JSON)
                 .header("Authorization","Bearer " + bearerToken)
                 .when().get(endPoint).then().extract().response();
 
