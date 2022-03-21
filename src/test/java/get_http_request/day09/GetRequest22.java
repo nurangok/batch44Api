ed7e899a4d9ee51e82e6e4fc97ad238be23a4357
nurangok <nurlibreville@gmail.com>
2022-03-21 22:27:26 +0100

    herokuapp

--- src/test/java/get_http_request/day09/GetRequest22.java
+++ src/test/java/get_http_request/day09/GetRequest22.java
@@ -1,4 +1,17 @@
 package get_http_request.day09;
 
-public class GetRequest22 {
+import base_url.HerOkuAppBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+import test_data.HerOkuAppTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest22 extends HerOkuAppBaseUrl {
+
 }
