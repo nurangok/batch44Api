f17eb42bbed22d7d80b40408d06ab73f5ab03930
nurangok <nurlibreville@gmail.com>
2022-03-23 23:50:37 +0100

    get23

--- src/test/java/get_http_request/day10/GetRequest23.java
+++ src/test/java/get_http_request/day10/GetRequest23.java
@@ -1,4 +1,16 @@
 package get_http_request.day10;
 
+import base_url.DummyBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+import test_data.DummyTestData;
+
+import java.util.*;
+
+import static io.restassured.RestAssured.given;
+
 public class GetRequest23 {
+
 }
