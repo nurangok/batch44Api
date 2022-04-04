072391f936bd8df4e69cc899cbe837316b19f609
nurangok <nurlibreville@gmail.com>
2022-04-04 21:24:54 +0200

    m

--- src/test/java/pojos/BoookingDatesPojo.java
+++ src/test/java/pojos/BoookingDatesPojo.java
@@ -1,4 +1,55 @@
 package pojos;
 
+
+
 public class BoookingDatesPojo {
+    /*
+      "checkin": "2020-09-09",
+      "checkout": "2020-09-21"
+     */
+
+    //private değişkenler oluşturduk
+    private String checkin;
+    private String checkout;
+
+    //2-getter setter
+
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    //3- constructor oluştur, parametreli ve parametresiz
+
+
+    public BoookingDatesPojo() {
+    }
+
+    public BoookingDatesPojo(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    //toString metodu oluştur
+
+
+    @Override
+    public String toString() {
+        return "BookingDatesPojo{" +
+                "checkin='" + checkin + '\'' +
+                ", checkout='" + checkout + '\'' +
+                '}';
+    }
 }
