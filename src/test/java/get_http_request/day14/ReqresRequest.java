b0463b7788ba066c82e64c35795664ae7826d281
nurangok <nurlibreville@gmail.com>
2022-04-28 14:58:59 +0200

    request

--- src/test/java/get_http_request/day14/ReqresRequest.java
+++ src/test/java/get_http_request/day14/ReqresRequest.java
@@ -3,5 +3,14 @@ package get_http_request.day14;
 import org.junit.Test;
 
 public class ReqresRequest {
+    @Test
+    public void test(){
+
+        ReqresToken obje=new ReqresToken();
+        System.out.println(obje.TokenAl());
+
+
+
+    }
 
 }
